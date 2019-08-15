fb4498f0b2cb660624a3368fe30429f65336816a
Chris Abedi <cabedi@vertafore.com>
2019-08-15 15:52:31 -0600

    redo routes

--- src/main/java/cabedi/AnagramController.java
+++ src/main/java/cabedi/AnagramController.java
@@ -6,10 +6,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.Gson;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static javax.swing.UIManager.put;
 
 @RestController
 public class AnagramController implements ErrorController {
@@ -25,14 +28,11 @@ public class AnagramController implements ErrorController {
             put("DELETE /words.json", "Deletes all contents of the data store.");
         }
     };
-    ObjectMapper mapper = new ObjectMapper();
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/", produces= MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String,String> ApiHome() {
 
         dataStore = Utility.SetUpDataStore();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
 
         return routes;
 
@@ -41,9 +41,11 @@ public class AnagramController implements ErrorController {
     @PostMapping(value = "/words.json", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public void PostWords(@RequestBody final String json) throws IOException {
-
+        //I have messed this route up :)
         // Store words in HashMap equivalent of Json
-        JsonNode valuesNode = mapper.readTree(json).get("words");
+       // JsonNode valuesNode = objectMapper.readTree(json).get("words");
+        
+
 
         ArrayList<String> words = new ArrayList<>();
         for (JsonNode node : valuesNode) {
@@ -54,8 +56,8 @@ public class AnagramController implements ErrorController {
         dataStore.put(key, words);
     }
 
-    @RequestMapping(value = "/anagrams/{word}.json")
-    public ArrayList<String> GetAnagrams(@PathVariable("word") final String word, @RequestParam(required = false) final String limit) {
+    @RequestMapping(value = "/anagrams/{word}.json",produces=MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String, ArrayList<String>> GetAnagrams(@PathVariable("word") final String word, @RequestParam(required = false) final String limit) {
 
         String key = Utility.SortString(word);
             if (dataStore.get(key)!=null) {
@@ -66,16 +68,27 @@ public class AnagramController implements ErrorController {
                     max = Integer.parseInt(limit);
 
                 anagrams.remove(word);
-                return new ArrayList<String>(anagrams.subList(0,max));
+
+                ArrayList<String> returnJson = new ArrayList<String>(anagrams.subList(0,max));
+
+                return new HashMap<String,ArrayList<String>>(){
+                    {
+                        put("anagrams",returnJson);
+                    }
+                };
             }
             else
-                return new ArrayList<>();
+                return new HashMap<String,ArrayList<String>(){
+                    {
+                        put("anagrams", new ArrayList<String>());
+                    }
+                };
 
     }
 
     @DeleteMapping(value = "/words/{word}.json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ArrayList<String> DeleteAnagram(@PathVariable("word") final String word) {
+    public HashMap<String, ArrayList<String>> DeleteAnagram(@PathVariable("word") final String word) {
 
         String key = Utility.SortString(word);
         ArrayList<String> anagrams = dataStore.get(key);
@@ -84,17 +97,25 @@ public class AnagramController implements ErrorController {
             dataStore.put(key, anagrams);
         }
 
-        return anagrams;
+        return new HashMap<String,ArrayList<String>>(){
+            {
+                put("words",anagrams);
+            }
+        };
     }
 
     @DeleteMapping(value = "/words.json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ArrayList<String> DeleteAnagram() {
+    public HashMap<String,ArrayList<String>> DeleteAnagram() {
         //Remove all Words
         if (dataStore.size()>0){
             dataStore.clear();
         }
-            return new ArrayList<String>();
+            return new HashMap<String, ArrayList<String>>(){
+                {
+                    put("words", new ArrayList<String>());
+                }
+            };
     }
 
    // @RequestMapping(value="/error")
--- src/main/java/cabedi/App.java
+++ src/main/java/cabedi/App.java
@@ -4,12 +4,27 @@
 
 package cabedi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class App {
 
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+
+        return objectMapper;
+    }
     public static void main(String[] args) {
         System.setProperty("spring.jackson.serialization.INDENT_OUTPUT", "true");
         SpringApplication.run(App.class, args);
